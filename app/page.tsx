e0a9e4d7ff7b962bfb83dcb0097a9e1b42cd409e
Brandon Wong <brandonwongck@gmail.com>
2024-01-03 23:54:03 +0800

    fix responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -229,166 +229,167 @@ export default function Home() {
   return (
     <main className="flex flex-col gap-y-4 w-fit min-h-screen p-24 mx-auto content-center">
       <Header />
-      <div className="grid w-full max-w-sm items-center gap-2.5">
-        <div className="flex gap-x-2">
-          <Input
-            aria-label="Who's there? (min. 3 char)"
-            type="text"
-            id="Names"
-            placeholder="Name"
-            onKeyDown={onNamesHandleKeyDown}
-            onChange={onChangeSetName}
-            className="w-full"
-            value={name}
-            ref={inputRef}
-          />
-          <Button
-            variant="outline"
-            size="icon"
-            className="self-end aspect-square"
-            onClick={onClickAddNamesButton}
-          >
-            <PlusIcon className="h-4 w-4" />
-          </Button>
-          {editName && (
+      <div className="grid w-full items-center  place-items-center gap-2.5">
+        <div className="max-w-sm items-start grid gap-2.5">
+          <div className="flex gap-x-2 max-w-sm">
+            <Input
+              aria-label="Who's there? (min. 3 char)"
+              type="text"
+              id="Names"
+              placeholder="Name"
+              onKeyDown={onNamesHandleKeyDown}
+              onChange={onChangeSetName}
+              className="w-full"
+              value={name}
+              ref={inputRef}
+            />
             <Button
-              variant="destructive"
+              variant="outline"
               size="icon"
               className="self-end aspect-square"
-              onClick={handleRemoveName}
+              onClick={onClickAddNamesButton}
             >
-              <Cross1Icon className="h-4 w-4" />
+              <PlusIcon className="h-4 w-4" />
             </Button>
-          )}
-        </div>
-        <div className="flex gap-x-2">
-          <Input
-            aria-label="Total amount (aft. tax)"
-            type="number"
-            id="total"
-            placeholder="e.g. 420.69"
-            value={total}
-            onChange={onTotalOnChange}
-            onKeyDown={onTotalHandleKeyDown}
-          />
-
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label>{`Paid by`}</Label>
-            <Select onValueChange={onSelectPaidBy}>
-              <SelectTrigger className="w-full">
-                <SelectValue placeholder={"No one yet..."} />
-              </SelectTrigger>
-              <SelectContent>
-                {names.map((name) => {
-                  return (
-                    <SelectItem key={name} value={name}>
-                      {name}
-                    </SelectItem>
-                  );
-                })}
-              </SelectContent>
-            </Select>
+            {editName && (
+              <Button
+                variant="destructive"
+                size="icon"
+                className="self-end aspect-square"
+                onClick={handleRemoveName}
+              >
+                <Cross1Icon className="h-4 w-4" />
+              </Button>
+            )}
           </div>
-        </div>
-        <div className="items-center">
-          <div className="flex gap-x-2">
-            {names.length > 0 && (
-              <div className="self-start w-full max-w-[100px]">
-                <Label>{`Shared by`}</Label>
-                <ToggleGroup
-                  type="multiple"
-                  className="self-end gap-2 flex-wrap"
-                  onValueChange={(value) => {
-                    if (value) setSharedBy(value);
-                  }}
-                >
-                  {names.map((n) => {
+          <div className="flex gap-x-2 max-w-sm">
+            <Input
+              aria-label="Total amount (aft. tax)"
+              type="number"
+              id="total"
+              placeholder="e.g. 420.69"
+              value={total}
+              onChange={onTotalOnChange}
+              onKeyDown={onTotalHandleKeyDown}
+            />
+            <div className="grid w-full items-center gap-1.5">
+              <Label>{`Paid by`}</Label>
+              <Select onValueChange={onSelectPaidBy}>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder={"No one yet..."} />
+                </SelectTrigger>
+                <SelectContent>
+                  {names.map((name) => {
                     return (
-                      <ToggleGroupItem
-                        value={n}
-                        aria-label={`Toggle ${n}`}
-                        key={n}
-                        className="w-fit aspect-square"
-                      >
-                        {n.split(" ").length > 1
-                          ? n
-                              .split(" ")
-                              .map((word) => word.charAt(0))
-                              .join("")
-                          : n.substring(0, 3)}
-                      </ToggleGroupItem>
+                      <SelectItem key={name} value={name}>
+                        {name}
+                      </SelectItem>
                     );
                   })}
-                </ToggleGroup>
-              </div>
-            )}
-            <div className="flex gap-x-2 items-center">
-              <Input
-                aria-label="What's on the bill? (bef. tax)"
-                type="number"
-                id="total"
-                placeholder={
-                  totalSum
-                    ? Math.round(
-                        total -
-                          totalSum * (1 + serviceTax / 100) * (1 + gst / 100)
-                      ).toString()
-                    : "e.g. 420.69"
-                }
-                onKeyDown={onItemsHandleKeyDown}
-                onChange={onChangeSetCurrentItem}
-                value={currentItem}
-                ref={itemRef}
-              />
-              <Button
-                variant="outline"
-                size="icon"
-                className="aspect-square self-center mt-5"
-                onClick={onClickAddItemsButton}
-                disabled={!sharedBy.length}
-              >
-                <PlusIcon className="h-4 w-4" />
-              </Button>
-              {editCurrentItem !== 0 && (
-                <Button
-                  variant="destructive"
-                  size="icon"
-                  className="aspect-square self-center mt-5"
-                  onClick={handleRemoveItem}
-                >
-                  <Cross1Icon className="h-4 w-4" />
-                </Button>
-              )}
+                </SelectContent>
+              </Select>
             </div>
           </div>
-          <Accordion type="single" collapsible>
-            <AccordionItem value="item-1">
-              <AccordionTrigger>Need to change the taxes?</AccordionTrigger>
-              <AccordionContent className="flex gap-x-2 mt-2">
+          <div className="items-center max-w-sm">
+            <div className="flex gap-x-2">
+              {names.length > 0 && (
+                <div className="self-start w-full max-w-[140px]">
+                  <Label>{`Shared by`}</Label>
+                  <ToggleGroup
+                    type="multiple"
+                    className="self-end gap-2 flex-wrap"
+                    onValueChange={(value) => {
+                      if (value) setSharedBy(value);
+                    }}
+                  >
+                    {names.map((n) => {
+                      return (
+                        <ToggleGroupItem
+                          value={n}
+                          aria-label={`Toggle ${n}`}
+                          key={n}
+                          className="w-fit aspect-square"
+                        >
+                          {n.split(" ").length > 1
+                            ? n
+                                .split(" ")
+                                .map((word) => word.charAt(0))
+                                .join("")
+                            : n.substring(0, 3)}
+                        </ToggleGroupItem>
+                      );
+                    })}
+                  </ToggleGroup>
+                </div>
+              )}
+              <div className="flex gap-x-2 items-center">
                 <Input
-                  aria-label="Service tax (%)"
+                  aria-label="What's on the bill? (bef. tax)"
                   type="number"
-                  id="serviceTax"
-                  placeholder="e.g. 10"
-                  onChange={onChangeServiceTax}
-                  className="w-full"
-                  value={serviceTax}
+                  id="total"
+                  placeholder={
+                    totalSum
+                      ? Math.round(
+                          total -
+                            totalSum * (1 + serviceTax / 100) * (1 + gst / 100)
+                        ).toString()
+                      : "e.g. 420.69"
+                  }
+                  onKeyDown={onItemsHandleKeyDown}
+                  onChange={onChangeSetCurrentItem}
+                  value={currentItem}
+                  ref={itemRef}
                 />
-                <Input
-                  aria-label="GST (%)"
-                  type="number"
-                  id="gst"
-                  placeholder="e.g. 9"
-                  onChange={onChangeGst}
-                  value={gst}
-                  className="w-full"
-                />
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="aspect-square self-center mt-5"
+                  onClick={onClickAddItemsButton}
+                  disabled={!sharedBy.length}
+                >
+                  <PlusIcon className="h-4 w-4" />
+                </Button>
+                {editCurrentItem !== 0 && (
+                  <Button
+                    variant="destructive"
+                    size="icon"
+                    className="aspect-square self-center mt-5"
+                    onClick={handleRemoveItem}
+                  >
+                    <Cross1Icon className="h-4 w-4" />
+                  </Button>
+                )}
+              </div>
+            </div>
+            <Accordion type="single" collapsible>
+              <AccordionItem value="item-1">
+                <AccordionTrigger>Need to change the taxes?</AccordionTrigger>
+                <AccordionContent className="flex gap-x-2 mt-2">
+                  <Input
+                    aria-label="Service tax (%)"
+                    type="number"
+                    id="serviceTax"
+                    placeholder="e.g. 10"
+                    onChange={onChangeServiceTax}
+                    className="w-full"
+                    value={serviceTax}
+                  />
+                  <Input
+                    aria-label="GST (%)"
+                    type="number"
+                    id="gst"
+                    placeholder="e.g. 9"
+                    onChange={onChangeGst}
+                    value={gst}
+                    className="w-full"
+                  />
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </div>
         </div>
         {names.length > 0 && (
-          <Card>
+          <Card className="">
             <CardHeader>
               <CardTitle className="flex gap-x-2">
                 {names.map((n) => {
